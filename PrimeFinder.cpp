102f62e4deebf835760800cdffcec25202e7d65a
jegunderson <jegunderson@byui.edu>
2023-06-26 16:41:42 -0600

    Add project files.

---
+++ PrimeFinder.cpp
@@ -0,0 +1,121 @@
+// PrimeFinder.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n)
+{
+    // 0, 1, and negatives are not prime
+    if (n <= 1)
+    {
+        return false;
+    }
+    // All factors are less than sqrt
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+// int1 can't be more than 7 digits
+int concatenate(int int1, int int2)
+{
+    int int2_copy = int2;
+    do
+    {
+        int1 *= 10;
+        int2_copy /= 10;
+    } while (int2_copy);
+
+    return int1 + int2;
+    
+}
+
+bool isNextNumber(string str, int index)
+{
+    if (isdigit(str[index])) return true;
+
+    else return false;
+}
+
+vector<int> primeNumberPrinter(string str)
+{
+    vector<int> primes;
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        char c = str[i];
+        if (isdigit(c))
+        {
+            // Turn char into int
+            int currentNumber = c - '0';
+
+            // If prime add it to primes list
+            if (isPrime(currentNumber))
+            {
+                primes.push_back(currentNumber);
+            }
+            // Counter for while loop
+            int counter = i + 1;
+            while (isNextNumber(str, counter))
+            {
+                int nextNumber = str[counter] - '0';
+                currentNumber = concatenate(currentNumber, nextNumber);
+
+                if (isPrime(currentNumber))
+                {
+                    primes.push_back(currentNumber);
+                }
+                counter += 1;
+            }
+        }
+    }
+    return primes;
+}
+
+int main()
+{
+    string str = "abc2134kd31";
+    vector<int> primes = primeNumberPrinter(str);
+
+    for (const auto& num : primes) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    /*********************************
+    //Test Concatenate
+    int num1 = 45474534;
+    int num2 = 3
+        ;
+    cout << concatenate(num1, num2);
+    ********************************/
+
+    /*****************************************
+    //Test is Prime
+    cout << sqrt(3) << endl;
+    cout << 3 % 2 << endl;
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (isPrime(i))
+        {
+            cout << i << endl;
+            printf("printf true : %d\n", true);
+        }
+        else
+        {
+            cout << i << endl;
+            printf("printf false: %d\n", false);
+        }
+    }
+    **********************************************/
+   
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+# Overview
+
+I wanted a coding challenge. Since I know C# I wanted to try a similar language to get a wider range of experience. I created a program that will take a string of letters and numbers, 
+and find all the prime numbers throughout.
+
+
+
+{Provide a link to your YouTube demonstration. It should be a 4-5 minute demo of the software running and a walkthrough of the code. Focus should be on sharing what you learned about the language syntax.}
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+
+## Development Tools
+Visual Studio
+## Language
+C++
+# Useful Websites
+
+- [W3schools](https://www.w3schools.com/cpp/cpp_intro.asp#:~:text=C%2B%2B%20is%20an%20object%2Doriented,fun%20and%20easy%20to%20learn!)
+- [Sentry](https://sentry.io/answers/char-to-int-in-c-and-cpp/#:~:text=C%20and%20C%2B%2B%20store%20characters,the%20value%20of%20'0'%20.)
+
+# Future Work
+
+{Make a list of things that you need to fix, improve, and add in the future.}
+
+- Allow user to input string
+- Find way to concatenate numbers more than 7 digits
+- Save previously found solutions and strings to file or database
